44d58d5504ebd522a3867afec4dbee9baabaf235
Nate Miller <natekmiller96@gmail.com>
2018-10-10 18:05:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  handlebarsSetup()
+  initForm()
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
